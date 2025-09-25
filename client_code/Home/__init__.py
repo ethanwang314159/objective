20e219ce65b97d9cd24d356d72690e6b9ca0bb81
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-25 22:14:13 +0000

    Edited Account, Home, Home.ItemTemplate1, Subject and 2 more

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,6 +1,10 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import datetime
+from anvil import Label, ColumnPanel, GridPanel
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
@@ -14,6 +18,15 @@ class Home(HomeTemplate):
       self.title_label.text = "Good afternoon."
     else:
       self.title_label.text = "Good night."
+    print('hi')
+    p_labels = [r['period'] for r in app_tables.my_table.search()]
+    print(p_labels)
+    for p_label in p_labels:
+      new_label = Label(text="p_label",
+                        foreground="blue",
+                        font_size=20)
+      self.column_panel_1.add_component(new_label)
+    
     
   def button_1_click(self, **event_args):
     open_form('Home')
