18d76e755408dfa25313cc11853e4a85e8d64dae
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-25 15:39:23 +0000

    Created Home.ItemTemplate1; Edited Home (greeting)

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,10 +1,15 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
-
+import datetime
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
+    current_hour = datetime.datetime.now().hour
+    if current_hour < 12:
+      self.title_label.text = "Good morning"
+    else:
+      self.title_label.text = "Good afternoon"
     
   def button_1_click(self, **event_args):
     open_form('Home')
