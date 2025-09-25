af0a14e1ad50c0c28af34af25a166cfa6aab30df
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-25 04:17:30 +0000

    Edited Subject

--- client_code/Subject/__init__.py
+++ client_code/Subject/__init__.py
@@ -4,11 +4,5 @@ from anvil import *
 
 class Subject(SubjectTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
-  def button_4_click(self, **event_args):
-    target_url = "/Account"  # Replace with your desired URL
-    anvil.js.window.open(target_url, "_self")
