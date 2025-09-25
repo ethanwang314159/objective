c503719dce3f936a7889a681da6ade23dd2f7f7d
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-25 15:10:08 +0000

    Working routing

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -10,5 +10,13 @@ class Home(HomeTemplate):
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+    open_form('Home')
+
+  def button_2_click(self, **event_args):
+    open_form('Timetable')
+  
+  def button_3_click(self, **event_args):
+    open_form('Subjects')
+
+  def button_4_click(self, **event_args):
+    open_form('Account')
