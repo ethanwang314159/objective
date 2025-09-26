3d04cd305b9e96f896a92f33fc4a7c8d42225dae
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-26 23:52:34 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -65,7 +65,8 @@ class Home(HomeTemplate):
           print(ptime)
           print(minutes_since_midnight)
           readable_diff_label = Label(text="Completed",
-                                      role="headline")
+                                      role="headline"
+                                      font_size=20)
 
         self.grid_panel_2.add_component(time_label)
         self.grid_panel_3.add_component(readable_diff_label)
