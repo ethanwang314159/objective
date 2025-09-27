c4cfa8137b801b6020c67544337e035097b786b6
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-27 23:37:50 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -62,7 +62,7 @@ class Home(HomeTemplate):
             readable_diff_hour = ''
           else:
             readable_diff_hour = str(diff_minutes // 60) + 'h'
-          readable_diff_sec = str(diff_minutes % 60)#.rjust(2, '0')
+          readable_diff_sec = str(diff_minutes % 60) + 'min'
           readable_diff = "{} {}".format(readable_diff_hour, readable_diff_sec)
           readable_diff_label = Label(text=readable_diff,
                                       role="headline",
