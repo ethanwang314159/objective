53a4f04e07190b441bf20dae2d4d993938e6d9a8
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-10-12 08:48:24 +0000

    Edited settings and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,106 +1,63 @@
 from ._anvil_designer import HomeTemplate
-from ._anvil_designer import HomeTemplate
 from anvil import *
 from anvil.tables import app_tables
 from datetime import datetime
-from anvil import Label, ColumnPanel, GridPanel
-import anvil
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
     self.storage = []
-    current_hour = datetime.now().hour
-    if current_hour < 5:
+
+    hour = datetime.now().hour
+    if hour < 5:
       self.title_label.text = "Sleep."
-    elif current_hour < 12:
+    elif hour < 12:
       self.title_label.text = "Good morning!"
-    elif current_hour < 18:
+    elif hour < 18:
       self.title_label.text = "Good afternoon."
     else:
       self.title_label.text = "Good night."
-    
-    current_day_of_week = datetime.now().weekday()
-    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    self.label_1.text = days_of_week[current_day_of_week]
-    self.p_labels = [i['period'] for i in app_tables.monday.search()]
-    self.p_times = [i['start_min'] for i in app_tables.monday.search()]
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    self.label_1.text = days[datetime.now().weekday()]
+
+    records = list(app_tables.monday.search())
+    self.p_labels = [r['period'] for r in records]
+    self.p_times = [r['start_min'] for r in records]
+
     self.updateTimes()
 
   def updateTimes(self):
-    
     try:
-      # Fetch only a limited number of records (e.g., first 10)
-      anvil.server.reset_session()
-      p_labels = self.p_labels
-      p_times = self.p_times
+      self.grid_panel_1.clear()
       self.grid_panel_2.clear()
       self.grid_panel_3.clear()
-      self.grid_panel_1.clear()
-      for k in range(len(p_labels)):
-        new_label = Label(text=p_labels[k],
-                          role="headline",
-                          font_size=25
-                         )
-        self.grid_panel_1.add_component(new_label)
 
-      for j in range(len(p_times)):
-        ptime = p_times[j]
-        readable_hr = str(ptime // 60).rjust(2, '0')
-        readable_min = str(ptime % 60).rjust(2, '0')
-        readable_time = "{}:{}".format(readable_hr, readable_min)
-        time_label = Label(text=readable_time,
-                           role="headline",
-                           font_size=25
-                          )
+      now = datetime.now()
+      minutes_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).seconds // 60
 
-        now = datetime.now()
-        midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        time_since_midnight = now - midnight
-        minutes_since_midnight = int(time_since_midnight.total_seconds()) // 60
-        diff_minutes = ptime - minutes_since_midnight
-        if diff_minutes > 0:
-          if (int(diff_minutes // 60) == 0):
-            readable_diff_hour = ''
-          else:
-            readable_diff_hour = str(diff_minutes // 60) + 'h '
-          if diff_minutes % 60 == 0:
-            readable_diff_min = ''
-          else:
-            readable_diff_min = str(diff_minutes % 60) + 'min'
-          readable_diff = readable_diff_hour + readable_diff_min
-          readable_diff_label = Label(text=readable_diff,
-                                      role="headline",
-                                      font_size=25
-                                     )
+      for period, start_min in zip(self.p_labels, self.p_times):
+        self.grid_panel_1.add_component(Label(text=period, role="headline", font_size=25))
+
+        start_str = f"{start_min//60:02d}:{start_min%60:02d}"
+        self.grid_panel_2.add_component(Label(text=start_str, role="headline", font_size=25))
+
+        diff = start_min - minutes_since_midnight
+        if diff > 0:
+          hours, mins = divmod(diff, 60)
+          diff_text = (f"{hours}h " if hours else "") + (f"{mins}min" if mins else "")
         else:
-          readable_diff_label = Label(text="Completed",
-                                      role="headline",
-                                      font_size=25)
+          diff_text = "Completed"
 
-        self.grid_panel_2.add_component(time_label)
-        self.grid_panel_3.add_component(readable_diff_label)
+        self.grid_panel_3.add_component(Label(text=diff_text, role="headline", font_size=25))
 
     except Exception as e:
       print(f"Error retrieving records: {e}")
-    
-  def button_1_click(self, **event_args):
-    open_form('Home')
 
-  def button_2_click(self, **event_args):
-    open_form('Timetable')
-
-  def button_3_click(self, **event_args):
-    open_form('Subject')
-
-  def button_4_click(self, **event_args):
-    open_form('Account')
-
-  def link_1_click(self, **event_args):
-    open_form('Home')
-
-  def timer_1_tick(self, **event_args):
-    self.updateTimes()
-
-  def button_5_click(self, **event_args):
-    open_form('About')
+  def button_1_click(self, **event_args): open_form('Home')
+  def button_2_click(self, **event_args): open_form('Timetable')
+  def button_3_click(self, **event_args): open_form('Subject')
+  def button_4_click(self, **event_args): open_form('Account')
+  def button_5_click(self, **event_args): open_form('About')
+  def link_1_click(self, **event_args): open_form('Home')
+  def timer_1_tick(self, **event_args): self.updateTimes()
